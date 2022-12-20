b21c192a11752bf76e2e629645cf971d409c9d0a
Ivan Grekov <ivan_grekov@yahoo.com>
2022-12-20 22:06:26 +0300

    refactor: add file app tsx file

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.scss";
+import AppRouter from "./components/AppRouter";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Hello word</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
