726beb1c58aed4cbd9a10255f69ddea36ce4d51e
LeonID <lmatskevich79@gmail.com>
2022-12-26 00:48:18 +0300

    feat: add files Main

---
+++ src/components/Template/Main/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import '../../../assets/scss/main.scss';
+import './style.scss';
+
+function Main() {
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="main__wrapper"></div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
