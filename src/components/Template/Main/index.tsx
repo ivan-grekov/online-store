c1f5de712ac54eee65f5491f0c36a55f5d0b39b6
LeonID <lmatskevich79@gmail.com>
2022-12-26 02:10:09 +0300

    feat: add block aside & block content

--- src/components/Template/Main/index.tsx
+++ src/components/Template/Main/index.tsx
@@ -7,7 +7,36 @@ function Main() {
   return (
     <main className="main">
       <div className="container">
-        <div className="main__wrapper"></div>
+        <div className="main__wrapper">
+          <aside className="aside">
+            <div className="block block__category category">
+              <h3 className="category__title title">Category</h3>
+              <ul className="category__list">
+                <li className="category__item">
+                  <a href="" className="category__item-link link">
+                    electronics
+                  </a>
+                </li>
+                <li className="category__item">
+                  <a href="" className="category__item-link link">
+                    jewelery
+                  </a>
+                </li>
+                <li className="category__item">
+                  <a href="" className="category__item-link link">
+                    men's clothing
+                  </a>
+                </li>
+                <li className="category__item">
+                  <a href="" className="category__item-link link">
+                    women's clothing
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </aside>
+          <div className="content"></div>
+        </div>
       </div>
     </main>
   );
