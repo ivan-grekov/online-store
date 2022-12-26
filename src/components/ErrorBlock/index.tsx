4634243a46a32d1d3e795e5cfae0c9222bc38891
Ivan Grekov <ivan_grekov@yahoo.com>
2022-12-27 00:49:28 +0300

    feat: create component errorBlock

---
+++ src/components/ErrorBlock/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import styles from "./ErrorBlock.module.scss";
+
+function ErrorBlock() {
+  return (
+    <div className={styles.root}>
+      <h1 className={styles.root}>
+        <span className={styles.root}>😢</span>
+        <br />
+        Products not found :(
+      </h1>
+      <p className="description">
+        Unfortunately, this page is not available in our online-store
+      </p>
+    </div>
+  );
+}
+
+export default ErrorBlock;
