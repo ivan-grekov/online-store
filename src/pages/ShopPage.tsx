0a9ab8541f56e5543be81d3f181afdecd34c6468
Ivan Grekov <ivan_grekov@yahoo.com>
2022-12-31 11:45:29 +0300

    feat: add redux to ShopPage

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,71 +1,72 @@
-import * as React from "react";
-import CardSort from "../components/CardSort";
-import Categories from "../components/Categories";
-import Skeleton from "../components/ProductBlock/Skeleton";
-import ProductBlock from "../components/ProductBlock";
-import { IData } from "../types";
-
+import * as React from 'react';
+import CardSort from '../components/CardSort';
+import Categories from '../components/Categories';
+import Skeleton from '../components/ProductBlock/Skeleton';
+import ProductBlock from '../components/ProductBlock';
+import { IData } from '../types';
+import { SearchContext } from './../App';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../redux/store';
+import { setCategoryId } from '../redux/slices/filterSlice';
+import { setSortType } from '../redux/slices/filterSlice';
 const categories = [
-  "All",
-  "Electronics",
-  "Jewelery",
+  'All',
+  'Electronics',
+  'Jewelery',
   "Men's clothing",
   "Women's clothing",
 ];
 
-function ShopPage() {
+const ShopPage = () => {
+  const dispatch = useDispatch();
+  const { categoryId, sort } = useSelector((state: RootState) => state.filter);
+
+  const { searchValue } = React.useContext(SearchContext);
+
   const [products, setProducts] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-
-  const [sortType, setSortType] = React.useState({
-    name: "cost (DESC)",
-    sortProperty: "price",
-  });
-
-  const [categoryId, setCategoryId] = React.useState(0);
 
   React.useEffect(() => {
     setIsLoading(true);
 
-    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     fetch(
       `https://63a9d76d7d7edb3ae619c28b.mockapi.io/products${
-        categoryId ? `?category=${categories[categoryId]}` : "?"
-      }&sortBy=${sortType.sortProperty.replace("-", "")}&order=${order}`
+        categoryId ? `?category=${categories[categoryId]}` : '?'
+      }&sortBy=${sort.sortProperty.replace('-', '')}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
         setProducts(arr);
         setIsLoading(false);
       });
-  }, [categoryId, sortType]);
+  }, [categoryId, sort.sortProperty, searchValue]);
+
+  const prod = products.map((product: IData) => (
+    <ProductBlock {...product} key={product.id} />
+  ));
+
   return (
     <>
       <aside className="filters-block">
-        <CardSort
-          value={sortType}
-          onChangeSort={(obj: { name: string; sortProperty: string }) =>
-            setSortType(obj)
-          }
-        />
+        <CardSort />
       </aside>
       <section className="section-shop">
         <Categories
           value={categoryId}
-          onChangeCategory={(i) => setCategoryId(i)}
+          onChangeCategory={(id) => dispatch(setCategoryId(id))}
         />
         <h2 className="section-shop__title">{categories[categoryId]}</h2>
         <div className="section-shop__items">
           {isLoading
             ? [...new Array(4)].map((_, i) => <Skeleton key={i} />)
-            : products.map((product: IData) => (
-                <ProductBlock {...product} key={product.id} />
-              ))}
+            : prod}
         </div>
       </section>
     </>
   );
-}
+};
 
 export default ShopPage;
