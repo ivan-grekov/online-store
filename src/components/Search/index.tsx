a0f6c60dfd31727298ef2507e1bb75132a4ca92a
Ivan Grekov <ivan_grekov@yahoo.com>
2022-12-31 11:44:08 +0300

    feat: add func Search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import styles from './Search.module.scss';
+import { FaSearch } from 'react-icons/fa';
+import { SearchContext } from '../../App';
 
 const Search = () => {
+  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+
   return (
-    <input className={styles.root} placeholder="Search product..."></input>
+    <div className={styles.root}>
+      <FaSearch className={styles.search__icon} />
+      <input
+        value={searchValue}
+        className={styles.input}
+        placeholder="Search product..."
+        onChange={(e) => setSearchValue(e.target.value)}
+      ></input>
+    </div>
   );
 };
 
