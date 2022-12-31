860d8de22cd88c324fb420d23355945946072a0d
Ivan Grekov <ivan_grekov@yahoo.com>
2022-12-31 15:09:37 +0300

    feat: add func onChangeInput

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import styles from './Search.module.scss';
 import { FaSearch } from 'react-icons/fa';
-import { SearchContext } from '../../App';
+import { setSearchValue } from '../../redux/slices/filterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
 
 const Search = () => {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const dispatch = useDispatch();
+  // const [value, setValue] = React.useState<string>('');
+  const { searchValue } = useSelector((state: RootState) => state.filter);
+
+  // const updateSearchValue = React.useCallback(
+  //   debounce((str: string) => {
+  //     dispatch(setSearchValue(str));
+  //   }, 150),
+  //   []
+  // );
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearchValue(event.target.value));
+    // updateSearchValue(event.target.value);
+  };
 
   return (
     <div className={styles.root}>
@@ -13,7 +29,7 @@ const Search = () => {
         value={searchValue}
         className={styles.input}
         placeholder="Search product..."
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={onChangeInput}
       ></input>
     </div>
   );
