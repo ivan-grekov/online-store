b8f2cd3c08dad7a4f4327c2568beeff3b6d26162
Ivan Grekov <ivan_grekov@yahoo.com>
2022-12-31 16:09:48 +0300

    feat: update search value with debounce

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,31 +2,30 @@ import React from 'react';
 import styles from './Search.module.scss';
 import { FaSearch } from 'react-icons/fa';
 import { setSearchValue } from '../../redux/slices/filterSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
+import { useDispatch } from 'react-redux';
+import debounce from 'lodash.debounce';
 
 const Search = () => {
   const dispatch = useDispatch();
-  // const [value, setValue] = React.useState<string>('');
-  const { searchValue } = useSelector((state: RootState) => state.filter);
+  const [value, setValue] = React.useState<string>('');
 
-  // const updateSearchValue = React.useCallback(
-  //   debounce((str: string) => {
-  //     dispatch(setSearchValue(str));
-  //   }, 150),
-  //   []
-  // );
+  const updateSearchValue = React.useCallback(
+    debounce((str: string) => {
+      dispatch(setSearchValue(str));
+    }, 250),
+    []
+  );
 
   const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSearchValue(event.target.value));
-    // updateSearchValue(event.target.value);
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
   };
 
   return (
     <div className={styles.root}>
       <FaSearch className={styles.search__icon} />
       <input
-        value={searchValue}
+        value={value}
         className={styles.input}
         placeholder="Search product..."
         onChange={onChangeInput}
