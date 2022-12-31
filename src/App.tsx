063882036e4038dcec7861daf063a9f32c531b0c
Ivan Grekov <ivan_grekov@yahoo.com>
2022-12-31 11:37:13 +0300

    feat: add context for func App

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,37 @@ import ShopPage from './pages/ShopPage';
 import ErrorPage from './pages/ErrorPage';
 import { Routes, Route } from 'react-router-dom';
 import BasketPage from './pages/BasketPage';
+import CardPage from './pages/CardPage';
+
+import type { RootState } from './redux/store';
+import { useSelector, useDispatch } from 'react-redux';
+
+interface ContextState {
+  searchValue: string;
+  setSearchValue: (param: string) => void;
+}
+
+export const SearchContext = React.createContext({} as ContextState);
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+
   return (
-    <div className="wrapper">
-      <Header />
-      <main>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<ShopPage />} />
-            <Route path="/basket" element={<BasketPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </div>
-      </main>
-    </div>
+    <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+      <div className="wrapper">
+        <Header />
+        <main>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<ShopPage />} />
+              <Route path="/basket" element={<BasketPage />} />
+              <Route path="/card" element={<CardPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </SearchContext.Provider>
   );
 }
 
