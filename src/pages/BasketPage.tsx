e4286b19a2f688f25ea4107976cefb980bf8022e
Ivan Grekov <ivan_grekov@yahoo.com>
2023-01-01 20:33:44 +0300

    feat: add markup and components for basket page

--- src/pages/BasketPage.tsx
+++ src/pages/BasketPage.tsx
@@ -1,19 +1,77 @@
-import * as React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import BasketImg from '../assets/svg/basket_img.svg';
+import BasketClear from '../assets/svg/basketClear_img.svg';
+import ButtonBack from '../assets/svg/button_back.svg';
 
-function BasketPage() {
+// import { BasketItem, BasketEmpty } from '../components';
+
+// import { selectBasket } from '../redux/cart/selectors';
+// import { clearItems } from '../redux/cart/slice';
+
+const BasketPage: React.FC = () => {
+  const dispatch = useDispatch();
+  // const { totalPrice, items } = useSelector(selectCart);
+
+  // const totalCount = items.reduce(
+  //   (sum: number, item: any) => sum + item.count,
+  //   0
+  // );
+
+  const onClickClear = () => {
+    if (window.confirm('Очистить корзину?')) {
+      // dispatch(clearItems());
+    }
+  };
+
+  // if (!totalPrice) {
+  //   // return <CartEmpty />;
+  // }
+
   return (
-    <div className="basket__wrapper">
-      <h1 className="basket__title">Basket</h1>
-      <section className="basket__content">
-        <div className="basket__top">
-          <h2 className="basket__subtitle">Products in basket</h2>
-          <span className="count-of-products">items: 1</span>
-          <button>Clear basket</button>
+    <div className="container container--cart">
+      <div className="cart">
+        <div className="cart__top">
+          <img src={BasketImg} alt="basket image" />
+          <h2 className="content__title">Basket</h2>
+          <div onClick={onClickClear} className="cart__clear">
+            <img src={BasketClear} alt="basket-clear image" />
+            <span>Clear basket</span>
+          </div>
         </div>
-        <div className="content__items"></div>
-      </section>
+        <div className="content__items">
+          {/* {items.map((item: any) => (
+            <BasketItem key={item.id} {...item} />
+          ))} */}
+        </div>
+        <div className="cart__bottom">
+          <div className="cart__bottom-details">
+            <span>
+              {' '}
+              All products: <b>{10}</b>{' '}
+            </span>
+            <span>
+              {' '}
+              Order price: <b>{2250} ₽</b>{' '}
+            </span>
+          </div>
+          <div className="cart__bottom-buttons">
+            <Link
+              to="/"
+              className="button button--outline button--add go-back-btn"
+            >
+              <img src={ButtonBack} alt="button back symbol" />
+              <span>Come back</span>
+            </Link>
+            <div className="button pay-btn">
+              <span>Оплатить сейчас</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default BasketPage;
