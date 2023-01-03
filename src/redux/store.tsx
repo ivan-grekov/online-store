e9f9cb1e20b3ed0252fbd099ae6e020ad315974f
Ivan Grekov <ivan_grekov@yahoo.com>
2023-01-03 19:32:12 +0300

    feat: add product slice into redux store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,13 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filter from './slices/filterSlice';
 import basket from './basket/slice';
+import product from './product/slice';
+import { useDispatch } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
     filter,
     basket,
+    product,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
