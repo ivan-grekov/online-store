d705c42e8c41447b2f1bb56d0258cc3b994d5aa9
Ivan Grekov <ivan_grekov@yahoo.com>
2023-01-03 15:07:44 +0300

    feat: add func to add  product to basket

--- src/components/ProductBlock/index.tsx
+++ src/components/ProductBlock/index.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import { FaStar } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { selectBasketItemById } from '../../redux/basket/selectors';
+import { addItem } from '../../redux/basket/slice';
+import { BasketItem } from '../../redux/basket/types';
 
 interface ProductBlockProps {
   key: number;
   id: number;
   title: string;
+  description: string;
   price: number;
   image: string;
   rate: number;
   count: number;
 }
 
-const ProductBlock: React.FC<ProductBlockProps> = ({
+export const ProductBlock: React.FC<ProductBlockProps> = ({
   id,
   title,
+  description,
   price,
   image,
   rate,
-  count,
 }) => {
-  const [productCount, setProductCount] = React.useState(0);
+  const dispatch = useDispatch();
+  const basketItem = useSelector(selectBasketItemById(id));
+  const addedCount = basketItem ? basketItem.count : 0;
+
+  const onClickAdd = () => {
+    const item: BasketItem = {
+      id,
+      title,
+      description,
+      price,
+      image,
+      count: 0,
+    };
+    dispatch(addItem(item));
+  };
+
   return (
     <div className="product-block">
       <div className="rate">
@@ -29,12 +50,14 @@ const ProductBlock: React.FC<ProductBlockProps> = ({
         </span>
         <span>{rate}</span>
       </div>
-      <img className="product-block__image" src={image} alt="image" />
-      <h4 className="product-block__title">{title}</h4>
+      <Link key={id} to={`/product/${id}`}>
+        <img className="product-block__image" src={image} alt="image" />
+        <h4 className="product-block__title">{title}</h4>
+      </Link>
       <div className="product-block__bottom">
         <div className="product-block__price">$ {price}</div>
         <button
-          onClick={() => setProductCount(productCount + 1)}
+          onClick={onClickAdd}
           className="button button--outline button--add"
         >
           <svg
@@ -50,7 +73,7 @@ const ProductBlock: React.FC<ProductBlockProps> = ({
             />
           </svg>
           <span>Add</span>
-          <i>{productCount}</i>
+          {addedCount > 0 && <i>{addedCount}</i>}
         </button>
       </div>
     </div>
