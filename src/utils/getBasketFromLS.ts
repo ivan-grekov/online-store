a304ffdb2365ad3d13cf0c9b36995781ad14fab5
Ivan Grekov <ivan_grekov@yahoo.com>
2023-01-03 15:03:49 +0300

    feat: add data to localStorage

--- src/utils/getBasketFromLS.ts
+++ src/utils/getBasketFromLS.ts
@@ -2,7 +2,7 @@ import { BasketItem } from '../redux/basket/types';
 import { calcTotalPrice } from './calcTotalPrice';
 
 export const getBasketFromLS = () => {
-  const data = localStorage.getItem('cart');
+  const data = localStorage.getItem('basket');
   const items = data ? JSON.parse(data) : [];
   const totalPrice = calcTotalPrice(items);
 
