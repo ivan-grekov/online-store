1e1302fbefc303a37a0a155b5d10a5f63f6320b8
LeonID <lmatskevich79@gmail.com>
2023-01-05 21:57:26 +0300

    feat: add files index.tsx & Footr.module.scss

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className="container"></div>
+    </footer>
+  );
+}
+
+export default Footer;
